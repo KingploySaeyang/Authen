132feebffb110de33921f83879364b1e13cc21cf
kingploy saeyang <652021042@tsu.ac.th>
2024-07-31 11:53:18 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const router = express.Router();
 
-
-const { getProducts, getProduct, createProduct, updateProduct, deleteProduct }
+const { getProduct, createProduct, updateProduct, deleteProduct }
 = require('../controllers/productController');
 
-const router = express.Router();
 router.get('/product', getProduct);
 router.post('/product', createProduct);
 router.put('/product/:id', updateProduct);
